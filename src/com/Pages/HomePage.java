e96737c95081453aa66df2da181f6c6e197ebbfd
Goni <goni@github.com>
2018-09-29 15:55:48 +0530

    New Url Removal Chnages

--- src/com/Pages/HomePage.java
+++ src/com/Pages/HomePage.java
@@ -15,7 +15,7 @@ import com.BaseSetup.BaseSetUp;
 
 public class HomePage extends BaseSetUp{
 	
-//	Sing In Elelments
+//	Sing In Elements
 	
 	String URL = "https://www.google.com/webmasters/tools/url-removal?hl=en&authuser=2&siteUrl=https://www.ipi-singapore.org/";
 	
@@ -41,6 +41,26 @@ public class HomePage extends BaseSetUp{
 	
 	By status = By.xpath("//span[@class='status-message-text']");
 	
+//	New Removal Console 
+	
+	String newUrl = "https://www.google.com/webmasters/tools/removals?pli=1";
+	
+	By newInputBox = By.xpath("//*[@class='gwt-TextBox jfk-textinput JX0GPIC-o-i']");
+	
+	By requestRemovalBtn = By.xpath("//div[@class='JX0GPIC-o-b']//button[@type='button']");
+	
+	By requestRemovalPopUpBtn = By.xpath("//*[@id='gwt-uid-134']");
+	
+	By cancelBtn = By.xpath("//*[@id='gwt-uid-134']");
+	
+	By errorMsg = By.xpath("//*[@class='gwt-Label goog-inline-block']");
+	
+	By thanksMsg = By.xpath("//*[@class='gwt-HTML']");
+	
+	By okBtn = By.xpath("//*[@id='gwt-uid-96']");
+	
+	
+	
 	
 	String home = System.getProperty("user.dir");
 	
@@ -51,7 +71,7 @@ public class HomePage extends BaseSetUp{
 		
 	}
 	
-	public void login(String UserName,String Pass) throws InterruptedException{
+	public void login(String UserName,String Pass,String url) throws InterruptedException{
 		
 		System.out.println("Clicking on Sign in Button");
 		
@@ -97,7 +117,7 @@ public class HomePage extends BaseSetUp{
 		
 		Thread.sleep(2000);
 		
-		driver.navigate().to(URL);
+		driver.navigate().to(url);
 		
 		Thread.sleep(2000);
 		
@@ -108,7 +128,7 @@ public class HomePage extends BaseSetUp{
 	@SuppressWarnings("resource")
 	public HomePage removeUrl(String UserName,String Pass) throws IOException, InterruptedException{
 		
-		login(UserName, Pass);
+		login(UserName, Pass,URL);
 		
 		Thread.sleep(2000);
 		
@@ -187,6 +207,101 @@ public class HomePage extends BaseSetUp{
 		
 		return new HomePage(driver);
 		
+	}
+	
+	
+//	 New URL Removal Console 
+	
+	@SuppressWarnings("resource")
+	public HomePage newUrlRemoveConsole(String UserName,String Pass) throws InterruptedException, IOException{
+		
+		login(UserName, Pass,newUrl);
+		
+		Thread.sleep(2000);
+		
+		File file = new File(home+"/TestData/ipi-path-urls.xlsx");
+		
+		FileInputStream fis = new FileInputStream(file);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		
+		XSSFSheet s  = wb.getSheetAt(0);
+		
+//		Workbook wb = new XSSFWorkbook(fis);
+//		
+//		Sheet s = wb.getSheet("URLs-for-Removal");
+		
+		int rowCount = s.getLastRowNum() - s.getFirstRowNum();
+		
+		for (int i = 0; i < rowCount+1; i++) {
+			
+			Row row = s.getRow(i);
+			
+			Cell cell  = row.getCell(0);
+			
+			String CellUrl = cell.getStringCellValue();
+			
+//			Command to Enter Url In that Box
+					
+			Thread.sleep(2000);
+			
+			System.out.println("Entering the URl : "+CellUrl);
+			
+			waitForClickabilityOf(newInputBox);
+			
+			driver.findElement(newInputBox).sendKeys(CellUrl);
+			
+			Thread.sleep(2000);
+			
+			System.out.println("Clicking on Removal Request Button");
+			
+			waitForClickabilityOf(requestRemovalBtn);
+			
+			driver.findElement(requestRemovalBtn).click();
+			
+			Thread.sleep(2000);
+			
+			System.out.println("Clicking on Removal Request Button");
+			
+			waitForClickabilityOf(requestRemovalPopUpBtn);
+			
+			driver.findElement(requestRemovalPopUpBtn).click();
+			
+	    	Thread.sleep(5000);
+	    	
+	    	try {
+	    		
+	    		boolean OK = driver.findElement(okBtn).isDisplayed();
+	    		
+	    		if (OK==true) {
+	    			
+	    			System.out.println("Clicking on OK Button");
+	    			
+	    			driver.findElement(okBtn).click();
+					
+				} else {
+					
+	    			System.out.println("Clicking on Cancel Button");
+	    			
+	    			driver.findElement(cancelBtn).click();
+
+				}
+				
+			} catch (Exception e) {
+				
+			}
+					
+			Thread.sleep(9000);
+					
+			
+		}
+		
+		System.out.println("Removed All Url");
+		
+		
+		
+		
+		return new HomePage(driver);
 	}
 	
 
